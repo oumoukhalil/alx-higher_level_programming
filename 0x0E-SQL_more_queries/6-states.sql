3f93c4f8f351ced426c5232e321cf4a6bfdefb57
oumoukhalil <zsarr2021@gmail.com>
2023-10-18 23:26:00 +0000

    0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- creates a database hbtn_0d_usa and the table states on your MySQL server.
+   -- states description:
+      -- id INT unique - auto generated, cant be null, is a primary key
+      -- name VARCHAR(256) - can't be null
+   -- If that table hbtn_0d_usa already exists, script should not fail
+   -- If that table states already exists, script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- lists those cities that contained in the database hbtn_0d_usa
+   -- Each record should display: cities.id - cities.name - states.name
+   -- Results will be sorted in ascending order by cities.id
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
