eec6eae9dc0de78626c7f4e01e9cc3a4c8f1749a
oumoukhalil <zsarr2021@gmail.com>
2023-11-08 09:47:15 +0000

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+""" A program that prints the ASCII alphabet, in lowercase,
+not followed by a new line."""
+for a in range(97, 123):
+    if a == 101 or a == 113:
+        continue
+    print("{:c}".format(a), end='')
