9834a8b3ac3375ab703fcd91f98e4cf9fe0fead2
oumoukhalil <zsarr2021@gmail.com>
2023-11-10 16:18:21 +0000

    0. Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to retrieve an element from a list like in C
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
+if __name__ != "__main__":
+    exit
 
 
-def element_at(my_list, idx):
-    """Retrieves an element from a list like in C
+def print_list_integer(my_list=[]):
+    """Function that prints all integers of a list.
 
     Args:
-        my_list: a list
-        idx: the index of item to retrieve
+        my_list: list of integers
 
     Returns:
-        the item at index idx
+        None
     """
-
-    # Check for negative and out of range index
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
-        return None
-    return my_list[idx]i
+    for each_int in my_list:
+        print("{:d}".format(each_int))
