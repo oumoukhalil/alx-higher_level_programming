d649a1f71d0f1f2a23bf96364465cdb354b0d751
oumoukhalil <zsarr2021@gmail.com>
2023-11-10 11:55:20 +0000

    2. How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
-    import sys
+    from sys import argv
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    if len(argv) == 1:
+        print(f"{len(argv) - 1} arguments.")
+    elif len(argv) == 2:
+        print(f"{len(argv) - 1} argument:")
+        print(f"{1}: {argv[1]}")
+    elif len(argv) > 2:
+        print(f"{len(argv) - 1} arguments:")
+        for i in range(1, len(argv)):
+            print(f"{i}: {argv[i]}")
