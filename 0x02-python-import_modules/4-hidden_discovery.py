71aa7016b6535012852a8a2b7b8aa7c007a4a99b
oumoukhalil <zsarr2021@gmail.com>
2023-11-10 14:07:30 +0000

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
-
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
-    else:
-        return(sub(a, b))
+if __name__ == "__main__":
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
