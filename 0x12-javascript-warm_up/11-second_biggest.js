4ba57cff357b7b280a03c2cb2aa5a2231dc84912
oumoukhalil <zsarr2021@gmail.com>
2023-11-15 17:44:53 +0000

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,19 @@
 #!/usr/bin/node
-//This  searches the second biggest integer in the list of arguments.
+/**
+ * script that searches the second biggest integer in the list of arguments.
+ */
+function secondBiggest (args) {
+  // convert all value in the list to integers
+  const integers = args.map(arg => +arg);
+  // remove all dublicates and sort in descending order
+  const sortedUniqueInt = [...new Set(integers)].sort((a, b) => b - a);
+  // check if the arguments are more than one
+  if (sortedUniqueInt.length < 2) {
+    return (0);
+  }
 
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  // return the second biggest integer
+  return (sortedUniqueInt[1]);
 }
+
+console.log(secondBiggest(process.argv.slice(2)));
