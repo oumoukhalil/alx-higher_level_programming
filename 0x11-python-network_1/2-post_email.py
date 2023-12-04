5e52757ada10bae03c176a5c4a6b2f02769194c0
oumoukhalil <zsarr2021@gmail.com>
2023-12-04 13:06:52 +0000

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
 """
-    Write a Python script that takes in a URL and an email, 
-    sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)
-"""
-
-
-import urllib.request
-import urllib.parse
-import sys
-"""import modules"""
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    value = {'email': email}
-    data = urllib.parse.urlencode(value)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        emailSend = response.read()
-        print("Your email is: {}".format(emailSend.decode('utf8')))
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    from sys import argv
+
+    value = {"email": argv[2]}
+    request = Request(
+            argv[1], urlencode(value).encode("ascii"))
+    with urlopen(request) as response:
+        head = response.headers.get('X-Request-Id')
+        print(response.read().decode('utf-8'))
