8f1a6dc6db0c6dc814120963dd08b6bf585ab0a1
oumoukhalil <fatsarr628@gmail.com>
2025-01-11 16:30:35 +0000

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # 5-no_c.py
 
-
+#!/usr/bin/python3
 def no_c(my_string):
-    """Remove all characters c and C from a string."""
- dict = {ord(char): None for char in "cC"}
-
-    # Call the translate string method giving it the mapping table
-    new_string = my_string.translate(dict)
-    return new_string
+    my_list = list(my_string)
+    size = len(my_list)
+    for i in range(size):
+        if my_list[i] == 'c' or my_list[i] == 'C':
+            my_list[i] = ''
+    return ("".join(my_list))
